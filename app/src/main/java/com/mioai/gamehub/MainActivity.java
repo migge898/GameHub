cbe4f9caae67e817227cf7007c4d7b426e3d2bb3
RuntimeException420 <jonashess@gmx.net>
2022-03-11 18:49:07 +0100

    Drawer created still not connected properly

--- app/src/main/java/com/mioai/gamehub/MainActivity.java
+++ app/src/main/java/com/mioai/gamehub/MainActivity.java
@@ -2,15 +2,24 @@ package com.mioai.gamehub;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TableLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -20,6 +29,11 @@ public class MainActivity extends AppCompatActivity
 
     private NavController.OnDestinationChangedListener listener;
 
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    private TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,15 +41,26 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         navController = Navigation.findNavController(this, R.id.fragment);
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        tabLayout = findViewById(R.id.tab_layout);
+//        navController = Navigation.findNavController(this, R.id.tab_layout);
         NavigationView navigationView = findViewById(R.id.navigationView);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        //Add new Fragments here
         appBarConfiguration = new AppBarConfiguration.Builder(R.id.firstFragment, R.id.secondFragment)
                 .setOpenableLayout(drawerLayout)
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(true);
+        toggle.syncState();
 
         listener = (controller, destination, arguments) ->
         {
@@ -66,5 +91,11 @@ public class MainActivity extends AppCompatActivity
         navController = Navigation.findNavController(this, R.id.fragment);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+        }
+        return true;
     }
 }
